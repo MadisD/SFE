9bc89a2950ce88160b3929fc2fcaceb3249e72b4
MadisD <madisdreifeld@gmail.com>
2015-05-26 00:55:03 +0300

    Added file compression to htaccess and minified cover.css

--- WebProject/application/views/pages/mainPageView.php
+++ WebProject/application/views/pages/mainPageView.php
@@ -64,11 +64,6 @@
 
 </div>
 
-<!-- Bootstrap core JavaScript
-================================================== -->
-<div class="footer">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-</div>
-<!-- Placed at the end of the document so the pages load faster -->
+
 </body>
 </html>
